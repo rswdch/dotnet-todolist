a2596695d7efd43f7448f5edfc96e2d24227a409
Ryan Sawadichai <ryan.sawadichai@outlook.com>
2023-07-14 14:50:39 -0600

    Model, ViewItem, Controller, DB

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,80 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.Sqlite;
+using Todo.Models;
+using Todo.Models.ViewModels;
+
+namespace Todo.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult Index()
+    {
+        var todoListViewModel = GetAllTodos();
+        return View(todoListViewModel);
+    }
+
+    internal TodoViewModel GetAllTodos()
+    {
+        List<TodoItem> todoList = new();
+        using (SqliteConnection con = new SqliteConnection("Data Source=db.sqlite"))
+        {
+            using (var tableCmd = con.CreateCommand())
+            {
+                con.Open();
+                tableCmd.CommandText = "SELECT * FROM todo";
+                
+                using (var reader = tableCmd.ExecuteReader())
+                {
+                    if (reader.HasRows)
+                    {
+                        while (reader.Read())
+                        {
+                            todoList.Add(new TodoItem
+                            {
+                                Id = reader.GetInt32(0),
+                                Name = reader.GetString(1)
+                            });
+                        }
+                    }
+                    return new TodoViewModel
+                    {
+                        TodoList = todoList
+                    };
+                }
+            }
+        }
+    }
+    // void -> no redirect, will go to /Home/Insert
+    // RedirectResult -> will go to a new URL in return
+    public RedirectResult Insert(TodoItem todo)
+    {
+    // Sqlite Connection is an unmanaged resource, which means it needs to be closed
+    // Using will close connection and command automatically
+        using (SqliteConnection con = new SqliteConnection("Data Source=db.sqlite"))
+        {
+            using (var tableCmd = con.CreateCommand())
+            {
+                con.Open();
+                tableCmd.CommandText = $"INSERT INTO todo (name) VALUES ('{todo.Name}')";
+                try
+                {
+                    // NonQuery means no return i.e. an insert
+                    tableCmd.ExecuteNonQuery();
+                }
+                catch(Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            }
+        }
+        return Redirect("http://localhost:5294/");
+    }
+}
---
+++ Models/ViewModels/TodoViewModel.cs
@@ -0,0 +1,7 @@
+namespace Todo.Models.ViewModels;
+
+public class TodoViewModel
+{
+    public List<TodoItem> TodoList { get; set; }
+    public TodoItem Todo { get; set; }
+}
