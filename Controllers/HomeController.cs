74836ac4ef796fc1b77e647d6736d3c458179b7f
Ryan Sawadichai <ryan.sawadichai@outlook.com>
2023-07-17 12:13:02 -0600

    styling

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -9,7 +9,6 @@ namespace Todo.Controllers;
 public class HomeController : Controller
 {
     private readonly ILogger<HomeController> _logger;
-
     public HomeController(ILogger<HomeController> logger)
     {
         _logger = logger;
@@ -67,6 +66,81 @@ public class HomeController : Controller
                 try
                 {
                     // NonQuery means no return i.e. an insert
+                    tableCmd.ExecuteNonQuery();
+                }
+                catch(Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            }
+        }
+        return Redirect("http://localhost:5294/");
+    }
+
+    public JsonResult Delete(int id)
+    {
+        using (SqliteConnection con = new SqliteConnection("Data Source=db.sqlite"))
+        {
+            using (var tableCmd = con.CreateCommand())
+            {
+                con.Open();
+                tableCmd.CommandText = $"DELETE from todo WHERE Id = '{id}'";
+                try
+                {
+                    tableCmd.ExecuteNonQuery();
+                }
+                catch(Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            }
+        }
+
+        return Json(new { });
+    }
+
+    [HttpGet]
+    public JsonResult PopulateForm(int id)
+    {
+        var todo = GetById(id);
+        return Json(todo);
+    }
+
+    internal TodoItem GetById(int id)
+    {
+        TodoItem todo = new();
+        
+        using (var con = new SqliteConnection("Data Source=db.sqlite"))
+        {
+            using (var tableCmd = con.CreateCommand())
+            {
+                con.Open();
+                tableCmd.CommandText = $"SELECT * from todo WHERE Id = '{id}'";
+                using (var reader = tableCmd.ExecuteReader())
+                {
+                    if (reader.HasRows)
+                    {
+                        reader.Read();
+                        todo.Id = reader.GetInt32(0);
+                        todo.Name = reader.GetString(1);
+                    }
+                }
+            }
+        }
+
+        return todo;
+    }
+
+    public RedirectResult Update(TodoItem todo)
+    {
+        using (SqliteConnection con = new SqliteConnection("Data Source=db.sqlite"))
+        {
+            using (var tableCmd = con.CreateCommand())
+            {
+                con.Open();
+                tableCmd.CommandText = $"UPDATE todo SET name = '{todo.Name}' WHERE Id = '{todo.Id}'";
+                try
+                {
                     tableCmd.ExecuteNonQuery();
                 }
                 catch(Exception ex)
--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,4 +1,30 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿function deleteTodo(i){
+    $.ajax({
+        url: 'Home/Delete',
+        type: 'POST',
+        data: {
+            id: i
+        },
+        success: () => {
+            window.location.reload();
+        }
+    });
+}
 
-// Write your JavaScript code.
+function populateForm(i){
+    $.ajax({
+        url: 'Home/PopulateForm',
+        type: 'GET',
+        data: {
+            id: i
+        },
+        dataType: 'json',
+        success: (response) => {
+            $("#Todo_Name").val(response.name);
+            $("#Todo_Id").val(response.id); 
+            $("#form-button").val("Update Todo");
+            $("#form-action").attr("action","/Home/Update")
+        }
+        
+    })
+}
